83956409a62bb224111bbab6290d6578760be4d0
drysm <oenpice2@gmail.com>
2021-08-03 22:36:08 +0900

    ch07.녹음기 녹음기능.. 뭔내용인지 모르겠다 UI 변경과 set 을 배운거 에 우선 만족

---
+++ app/src/main/java/com/example/recoder/RecordButton.kt
@@ -0,0 +1,30 @@
+package com.example.recoder
+
+import android.content.Context
+import android.util.AttributeSet
+import android.widget.ImageButton
+import androidx.appcompat.widget.AppCompatImageButton
+
+class RecordButton (
+    context: Context,
+    attrs: AttributeSet
+): AppCompatImageButton(context, attrs) {
+
+fun updateIconWithState(state: State) {
+    when(state) {
+        State.BEFORE_RECODING ->{
+            setImageResource(R.drawable.icl_record_24)
+        }
+        State.ON_RECORDING -> {
+            setImageResource(R.drawable.ic_stop_24)
+        }
+        State.AFTER_RECORDING -> {
+            setImageResource(R.drawable.ic_play_arrow_24)
+        }
+        State.ON_PLAYING -> {
+            setImageResource(R.drawable.ic_play_arrow_24)
+        }
+    }
+}
+
+}
---
+++ app/src/main/java/com/example/recoder/State.kt
@@ -0,0 +1,8 @@
+package com.example.recoder
+
+enum class State {
+    BEFORE_RECODING,
+    ON_RECORDING,
+    AFTER_RECORDING,
+    ON_PLAYING
+}
