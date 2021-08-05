f94ae87fad89abd52258e4e9d95d4be7ad0e1b97
drysm <oenpice2@gmail.com>
2021-08-05 22:08:44 +0900

    26:06 중간 듣기 근데 모이리 추가 된게 많지???
    나느 사운드 비져블라이저 만 추가 했는데...

---
+++ app/src/main/java/com/example/recoder/MainActivity.kt
@@ -0,0 +1,146 @@
+package com.example.recoder
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import java.util.jar.Manifest
+import android.*
+import android.content.pm.PackageManager
+import android.media.MediaPlayer
+import android.media.MediaRecorder
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+
+    private val resetButton: Button by lazy {
+        findViewById(R.id.resetbutton)
+    }
+
+    private val recordButton: RecordButton by lazy {
+        findViewById(R.id.recordbutton)
+    }
+
+
+    private val requeriedPermisions = arrayOf(android.Manifest.permission.RECORD_AUDIO)
+
+    private val recordingFilePath: String by lazy {
+        // 코틀린 스타일이 아님 externalCacheDir?.absolutePath + "/recording.3gp"
+        "${externalCacheDir?.absolutePath}/recording.3gp"
+    }
+
+    private var recorder: MediaRecorder? = null
+    private var player: MediaPlayer? = null
+    private var state = State.BEFORE_RECODING
+        set(value) {
+            field = value
+            resetButton.isEnabled = (value == State.AFTER_RECORDING) ||
+                    (value == State.ON_PLAYING)
+            recordButton.updateIconWithState(value)
+        }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        requestAudioPermission()
+        initViews()
+        bindViews()
+        initVariables()
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        val audioRecordPermissionGranted = requestCode == REQUSET_RECORD_AUDIO_PERMISSION &&
+                grantResults.firstOrNull() == PackageManager.PERMISSION_GRANTED
+
+        if (!audioRecordPermissionGranted) {
+            finish()
+        }
+
+    }
+
+    private fun requestAudioPermission() {
+
+        requestPermissions(requeriedPermisions, REQUSET_RECORD_AUDIO_PERMISSION)
+    }
+
+    fun initViews() {
+        recordButton.updateIconWithState(state)
+    }
+
+    //앱에 스테이트에 따른 유아이 구성 완료 --> 동작 구성 -->onclick listener을 받아야한다..
+    private fun bindViews() {
+        resetButton.setOnClickListener {
+            stopPlaying()
+            state = State.BEFORE_RECODING
+
+        }
+
+        recordButton.setOnClickListener {
+            when (state) {
+                State.BEFORE_RECODING -> {
+                    startRecording()
+                }
+                State.ON_RECORDING -> {
+                    stopRecording()
+                }
+                State.AFTER_RECORDING -> {
+                    startPlaying()
+                }
+                State.ON_PLAYING -> {
+                    stopPlaying()
+                }
+            }
+        }
+    }
+
+
+    private fun initVariables() {
+        state = State.BEFORE_RECODING
+    }
+    private fun startRecording() {
+
+        recorder = MediaRecorder().apply {
+            setAudioSource(MediaRecorder.AudioSource.MIC)
+            setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP)
+            setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB)
+            //따로 뭐 확인 안하겟지만 캐쉬 디렉토리...를 쓴데..
+            setOutputFile(recordingFilePath)
+            prepare()
+        }
+        recorder?.start()
+        state = State.ON_RECORDING
+    }
+
+    private fun stopRecording() {
+        recorder?.run {
+
+            stop()
+            release()
+        }
+        recorder = null
+        state = State.AFTER_RECORDING
+    }
+
+    private fun startPlaying() {
+        player = MediaPlayer()
+            .apply {
+                setDataSource(recordingFilePath)
+                prepare()  //ui 어씽크 ..prepare a singc
+            }
+        state = State.ON_RECORDING
+    }
+
+    private fun stopPlaying() {
+        player?.release()
+        player = null
+        state = State.AFTER_RECORDING
+    }
+
+    companion object {
+        private const val REQUSET_RECORD_AUDIO_PERMISSION = 222
+    }
+}
---
+++ app/src/main/java/com/example/recoder/SoundVisualizedView.kt
@@ -0,0 +1,81 @@
+package com.example.recoder
+
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.util.AttributeSet
+import android.view.View
+import java.util.*
+import kotlin.random.Random
+
+class SoundVisualizedView(
+    context: Context,
+    attrs: AttributeSet? = null
+) : View(context, attrs) {
+
+    val amplitudePaint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
+        color = context.getColor(R.color.purple_500)
+        strokeWidth = LINE_WIDTH
+        strokeCap = Paint.Cap.ROUND
+    }
+    var drawingWidth: Int = 0
+    var drawingHeigh: Int = 0
+    var drawingAmplitudes: List<Int> = emptyList()
+    // (0..10).map { Random.nextInt(Short.MAX_VALUE.toInt()) }
+    //emptyList()
+
+    private val visualizedRepeatAction: Runnable = object :Runnable {
+
+        override  fun run() {
+            //Amplitude , Draw
+
+
+
+            handler?.postDelayed(this, ACTION_INTERVAL)
+        }
+    }
+
+
+
+    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
+        super.onSizeChanged(w, h, oldw, oldh)
+        //이전 값은 필요없고 새로운 값을 집어 넣는다 .
+        drawingWidth = w
+        drawingHeigh = h
+    }
+
+    override fun onDraw(canvas: Canvas?) {
+        super.onDraw(canvas)
+
+        canvas ?: return
+
+        val centerY = drawingHeigh / 2f
+        var offsetX = drawingWidth.toFloat()
+
+        drawingAmplitudes.forEach { amplitude ->
+            val lineLength = amplitude / MAX_AMPLITUDE * drawingHeigh * 0.8F
+            offsetX -= LINE_SPACE
+            if (offsetX < 0) return@forEach
+
+            canvas.drawLine(
+                offsetX,
+                centerY - lineLength / 2f,
+                offsetX,
+                centerY + lineLength / 2f,
+                amplitudePaint
+            )
+        }
+
+
+    }
+
+
+    companion object {
+        private const val LINE_WIDTH = 10F
+        private const val LINE_SPACE = 15F
+
+        // float으로 나눗셈시 0이 되는 것을 막는다 .
+        private const val MAX_AMPLITUDE = Short.MAX_VALUE.toFloat()
+        private const val ACTION_INTERVAL = 20L
+    }
+}
